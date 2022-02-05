ad929ffcef298c64098b15c1eebcd6ce5ad76e7d
Alexandre Junior <38288419+AlexandreMMJr@users.noreply.github.com>
2022-02-05 10:40:37 -0300

    Delete math.js

--- math.js
+++
@@ -1,6 +0,0 @@
-const raio = 5.6;
-const area = Math.PI * Math.pow(raio, 2);
-
-//Acima usando exemplos do 'Math", com 'PI' e 'pow' de elevado
-console.log(area.toFixed(2));
-console.log(typeof Math); //Retorna 'object'
